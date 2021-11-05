e9578415f98d65d3eae512acf9d975e95e47e630
Jai Sawhney <jai.sawhney1@gmail.com>
2021-11-05 16:39:37 -0700

    Add login and signup routes

--- app.py
+++ app.py
@@ -1,13 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+from pymongo import MongoClient
 import os
+
+app = Flask(__name__)
+
+host = os.environ.get("MONGODB_URI", "mongodb://localhost:27017/Charity-tracker")
+client = MongoClient(host=host)
+db = client.get_default_database()
+
+users = db.users
+donations = db.donations
+charities = db.charities
 
 app = Flask(__name__)
 
 
 # Routes
 @app.route("/")
-def playlists_index():
+def index():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["GET"])
+def login():
+    return render_template("login.html")
+
+
+@app.route("/signup", methods=["GET"])
+def signup():
+    return render_template("signup.html")
+
+
+@app.route("/login", methods=["POST"])
+def login_submit():
+    return redirect(url_for("index"))
+
+
+@app.route("/register", methods=["POST"])
+def signup_submit():
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
